9fac3d63fd877b4e3b06e9504e7581b4079f1a29
Abdulrahman Aldayhani <148686321+Ab700700@users.noreply.github.com>
2023-12-18 14:50:04 +0300

    Update Place.java

--- capstone3/src/main/java/com/example/capstone3/Model/Place.java
+++ capstone3/src/main/java/com/example/capstone3/Model/Place.java
@@ -1,4 +1,29 @@
 package com.example.capstone3.Model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id ;
+    @NotEmpty(message = "Company name should not be empty")
+    @Column(columnDefinition = "varchar(50) not null unique")
+    private String companyName;
+    @NotEmpty(message = "Category should not be empty")
+    @Column(columnDefinition = "varchar(30) not null")
+    private String category;
+    @NotEmpty(message = "Description should not be empty")
+    @Column(columnDefinition = "varchar(200) not null")
+    private String description;
 }
